df05b7abe1b1fe546b6a3f5cea22206a7138f5b0
Mathu1015 <unora4281@gmail.com>
2026-04-27 22:47:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '26257599'))
 API_HASH = environ.get('API_HASH', 'cbdf90a6844992b76e143656f6e07562')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
-
+LOG_CHANNEL = None
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
